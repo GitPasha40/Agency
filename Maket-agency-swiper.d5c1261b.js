f4c864d904ebcf44ba565efcaff6a8bfbd5e443b
GitPasha40 <pavelsemus83@gmail.com>
2025-04-17 22:45:23 +0300

    Initial commit

---
+++ Maket-agency-swiper.d5c1261b.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".slider",{direction:"horizontal",loop:!0,speed:1e3,spaceBetween:10,effect:"cube",shadow:!0,shadowOffset:20,shadowScale:.94,slideShadows:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=Maket-agency-swiper.d5c1261b.js.map
